993fcbbc10bd5c0357fdb97825afc71c9371db66
alexandre-mace <contact.alexandremace@gmail.com>
2020-12-11 02:00:30 +0100

    Initialize client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import SwipeableViews from 'react-swipeable-views';
+const axios = require('axios');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const styles = {
+    slide: {
+        padding: 15,
+        minHeight: '100vh',
+        color: '#fff',
+        display: "flex",
+        position: "relative"
+    },
+    slide1: {
+    },
+    slide2: {
+    },
+    slide3: {
+    },
+};
+
+const App = () => {
+    const [view, setView] = useState(1);
+    const [loading, setLoading] = useState(true);
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://cors-anywhere.herokuapp.com/' + 'https://wd2q3hrfr4-rycbhpqkvnz7k.eu.s5y.io/posts')
+            .then(function (response) {
+                setPosts(response.data)
+                setLoading(false)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }, []);
+
+    return (
+        <div>
+            {loading && 'Chargement'}
+            {!loading &&
+                <div>
+                    {view <= posts.length &&
+                        <div className={"label"}>{view}/{posts.length}</div>
+                    }
+
+                    <SwipeableViews index={view - 1} onChangeIndex={(index, indexLatest, meta) => {
+                        setView(index + 1)
+                    }}>
+                        {posts.map((post) => (
+                            <div style={Object.assign({}, styles.slide)}>
+                                <div className={"m-auto"}>
+                                    <div className="card">
+                                        <div>{post.description}</div>
+                                        <br/><br/>
+                                        <a href={post.link}>{post.link}</a>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                        <div style={Object.assign({}, styles.slide)}>
+                            <div className={"m-auto"}>
+                                <div className="card d-flex">
+                                    <div className="m-auto">
+                                        À jour !
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </SwipeableViews>
+                </div>
+            }
+        </div>
+    )};
 
 export default App;
