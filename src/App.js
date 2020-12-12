e37421a69a4c51bff3e38cb8672b4e202d1ad08a
alexandre-mace <contact.alexandremace@gmail.com>
2020-12-12 02:13:59 +0100

    Update UI

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import SwipeableViews from 'react-swipeable-views';
 import { bindKeyboard } from 'react-swipeable-views-utils';
-const axios = require('axios');
+import {BottomNavigation, BottomNavigationAction, makeStyles, MobileStepper} from "@material-ui/core";
 
+const axios = require('axios');
+const useStyles = makeStyles({
+    root: {
+        borderTopLeftRadius: 16,
+        borderTopRightRadius: 16,
+        height: 70
+    },
+});
 const BindKeyboardSwipeableViews = bindKeyboard(SwipeableViews);
 
 const styles = {
     slide: {
-        padding: 15,
-        minHeight: '100vh',
+        marginTop: 20,
+        marginBottom: 50,
         color: '#fff',
         display: "flex",
         position: "relative"
@@ -22,13 +30,27 @@ const styles = {
     },
 };
 
+const getDomain = (url) => {
+    var matches = url.match(/^https?\:\/\/([^\/:?#]+)(?:[\/:?#]|$)/i);
+    var domain = matches && matches[1];  // domain will be null if no match is found
+
+    if (domain !== null) {
+        return domain
+    }
+
+    return 'lien invalide'
+}
+
 const App = () => {
+    const classes = useStyles();
+
     const [view, setView] = useState(1);
     const [loading, setLoading] = useState(true);
     const [posts, setPosts] = useState([]);
+    const [mode, setMode] = React.useState(0);
 
     useEffect(() => {
-        axios.get('https://cors-anywhere.herokuapp.com/' + 'https://wd2q3hrfr4-rycbhpqkvnz7k.eu.s5y.io/posts')
+        axios.get('https://wd2q3hrfr4-rycbhpqkvnz7k.eu.s5y.io/posts/latest')
             .then(function (response) {
                 setPosts(response.data)
                 setLoading(false)
@@ -40,24 +62,39 @@ const App = () => {
 
     return (
         <div>
-            {loading && 'Chargement'}
+            {loading && <div className={"overflow-hidden m-auto full-height d-flex"}><div className={"m-auto"}>Passion Béton</div></div>}
             {!loading &&
                 <div>
-                    {view <= posts.length &&
-                        <div className={"label"}>{view}/{posts.length}</div>
-                    }
-
+                    <div className={"header"}>
+                        <div className={"header-title"}>Cool de se revoir !</div>
+                        <div className={"header-subtitle"}>Voici les derniers partages par les passionés de béton.</div>
+                    </div>
+                    <MobileStepper
+                        variant="progress"
+                        steps={posts.length + 1}
+                        position="static"
+                        activeStep={view - 1}
+                        className={classes.root}
+                        style={{backgroundColor: 'transparent', borderRadius: 16, height: 20}}
+                        nextButton={
+                            <div>
+                            </div>
+                        }
+                        backButton={
+                            <div>
+                            </div>
+                        }
+                    />
                     <BindKeyboardSwipeableViews enableMouseEvents={true} index={view - 1} onChangeIndex={(index, indexLatest, meta) => {
                         setView(index + 1)
                     }}>
                         {posts.map((post, index) => (
                             <div key={index} style={Object.assign({}, styles.slide)}>
-                                <div className={"m-auto"}>
-                                    <div className="card">
-                                        <div>{post.description}</div>
-                                        <br/><br/>
-                                        <a href={post.link} target={"_blank"} rel={"noreferrer"}>{post.link}</a>
-                                    </div>
+                                <div className="card">
+                                    <div>{post.description}</div>
+                                    <br/><br/>
+                                    <a href={post.link} target={"_blank"} rel={"noreferrer"}><div className={"button" + (getDomain(post.link) === 'lien invalide' ? " disabled" : "")}>Lire</div></a>
+                                    <div className={"domain-helper"}>(vers {getDomain(post.link)})</div>
                                 </div>
                             </div>
                         ))}
@@ -71,7 +108,19 @@ const App = () => {
                             </div>
                         </div>
                     </BindKeyboardSwipeableViews>
+                    <BottomNavigation
+                        value={mode}
+                        onChange={(event, newValue) => {
+                            setMode(newValue);
+                        }}
+                        className={classes.root}
+                    >
+                        <BottomNavigationAction label={<div className={"dot"}></div>} icon={<div>News</div>} />
+                        <BottomNavigationAction label={<div className={"dot"}></div>} icon={<div>Archives</div>} />
+                        <BottomNavigationAction label={<div className={"dot"}></div>} icon={<div>Figures</div>} />
+                    </BottomNavigation>
                 </div>
+                
             }
         </div>
     )};
