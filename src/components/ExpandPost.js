e173a83337597f68703e1f77c72e2cb94f97a307
alexandre-mace <contact.alexandremace@gmail.com>
2020-12-20 22:21:25 +0100

    Fix expanded content on history posts

--- src/components/ExpandPost.js
+++ src/components/ExpandPost.js
@@ -4,6 +4,10 @@ import { useScrollConstraints } from "../utils/use-scroll-constraints";
 import { useWheelScroll } from "../utils/use-wheel-scroll";
 import { openSpring, closeSpring } from "../utils/cardAnimations";
 import Post from "./Post";
+import isImage from "../utils/isImage";
+import {apiAddress} from "../data/config/api";
+import DescriptionIcon from "@material-ui/icons/Description";
+import getDomain from "../utils/getDomain";
 
 const dismissDistance = 100;
 
@@ -40,6 +44,7 @@ const ExpandPost = ({post, small}) => {
     );
 
     y.set(0)
+    const domain = getDomain(post.link)
 
     return (
             <>
@@ -58,6 +63,31 @@ const ExpandPost = ({post, small}) => {
                         onUpdate={checkZIndex}
                     >
                         <Post small={small} post={post} isSelected={isSelected}/>
+                        {isSelected &&
+                        // <Scroll>
+                        //     <Frame>
+                        <div className={"card-fullcontent"}>
+                            <div style={{marginBottom: "1.6rem"}}>{post.description}</div>
+                            {isSelected && (
+                                <motion.div className={"content-container medias"}>
+                                    {post.medias.map((media, index) => (
+                                        <div key={index}>
+                                            {isImage(media.fileName) && (<div className={"media-image"}><img src={apiAddress + '/uploads/medias/' + media.fileName} alt=""/></div>)}
+                                            {!isImage(media.fileName) && (<DescriptionIcon/>)}
+                                        </div>
+                                    ))}
+                                </motion.div>
+                            )}
+                            <>
+                                <a href={post.link} target={"_blank"} rel={"noreferrer"}>
+                                    <div className={"button" + (domain === null ? " disabled" : "")}>Lire</div>
+                                </a>
+                                <div className={"domain-helper"}>({domain === null ? 'lien invalide' : domain})</div>
+                            </>
+                        </div>
+                            // </Frame></Scroll>
+
+                        }
                     </motion.div>
                 </div>
             </>
