0d581cb1f867dfc95540b24ef58233b5a6ce2b7e
alexandre-mace <contact.alexandremace@gmail.com>
2021-02-26 19:41:28 +0100

    Add safe fail on firebase execution

--- src/firebase.js
+++ src/firebase.js
@@ -12,29 +12,36 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
+
 const messaging = firebase.messaging();
 
 export const getToken = (setTokenFound) => {
-    return messaging.getToken({vapidKey: 'BIlSpicZrE17yN4cVKZ7xhD78z44qnnXlBH2kwR1trbSt1DHKP6oW4srqq4l6xD3GgKdThUYbcCGmoNvwxfdAJE'}).then((currentToken) => {
-        if (currentToken) {
-            setTokenFound(true);
-            addDeviceToken({'token': currentToken})
-            // Track the token -> client mapping, by sending to backend server
-            // show on the UI that permission is secured
-        } else {
-            console.log('No registration token available. Request permission to generate one.');
-            setTokenFound(false);
-            // shows on the UI that permission is required
-        }
-    }).catch((err) => {
-        console.log('An error occurred while retrieving token. ', err);
-        // catch error while creating client token
-    });
+    if (firebase.messaging.isSupported()) {
+        return messaging.getToken({vapidKey: 'BIlSpicZrE17yN4cVKZ7xhD78z44qnnXlBH2kwR1trbSt1DHKP6oW4srqq4l6xD3GgKdThUYbcCGmoNvwxfdAJE'}).then((currentToken) => {
+            if (currentToken) {
+                setTokenFound(true);
+                addDeviceToken({'token': currentToken})
+                // Track the token -> client mapping, by sending to backend server
+                // show on the UI that permission is secured
+            } else {
+                console.log('No registration token available. Request permission to generate one.');
+                setTokenFound(false);
+                // shows on the UI that permission is required
+            }
+        }).catch((err) => {
+            console.log('An error occurred while retrieving token. ', err);
+            // catch error while creating client token
+        });
+    } else {
+        return false
+    }
 }
 
 export const onMessageListener = () =>
     new Promise((resolve) => {
-        messaging.onMessage((payload) => {
-            resolve(payload);
-        });
+        if (firebase.messaging.isSupported()) {
+            messaging.onMessage((payload) => {
+                resolve(payload);
+            });
+        }
     });
